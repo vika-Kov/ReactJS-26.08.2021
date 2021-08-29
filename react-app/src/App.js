52b314f4782895ccf40a6a48bbfed7da86234bf3
repo <dragondark48@gmail.com>
2021-08-29 20:46:26 +0300

    lesson1 выполнение

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+
+const myName = 'Vika';  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message name={myName}/>
       </header>
     </div>
   );

5f9b865413e5a613a636cd087c3a479eec5b2f8d
repo <dragondark48@gmail.com>
2021-09-01 20:50:25 +0300

    lesson2 выполнен

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from './components/Message';
+import React, { useState, useEffect } from 'react';
 
-const myName = 'Vika';  
+const initPost = ["Hello, "]
 
 function App() {
+
+const [post, setPost]=useState(initPost)
+const [author, setAuthorName]=useState("")
+
+
+const updateName = (value)=>{
+  setAuthorName(value)
+}
+
+
+useEffect(() => {
+  setTimeout(loadMessage, 1500); 
+
+});
+
+const loadMessage = ()=>{
+  const placeHolder = document.querySelector("#robot");
+  placeHolder.innerHTML=post + author + "! I'm a Bot!";
+};
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Message name={myName}/>
+        <div id="robot"></div>
+        <br></br>
+        <Message updateName={setAuthorName}/>
       </header>
     </div>
   );

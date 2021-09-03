9e8aa10c280178ad341ec3b7daef14eb89e85548
repo <dragondark48@gmail.com>
2021-09-03 20:52:55 +0300

    lesson3

--- react-app/src/components/MessageList.js
+++ react-app/src/components/MessageList.js
@@ -1,17 +1,27 @@
 import React, {
     useState, useEffect
-} from 'react'
+} from 'react';
+import {
+    Button, 
+    TextField, 
+    List, 
+    ListItem,
+    ListItemText ,
+} from '@material-ui/core';
+
 
 
 export default function MessageList(){
     const [input, setInput]=useState("");
     const [name, setName] = useState("");
     const [messageList, setMessageList] = useState([{text:"Text", author:"Vika"}]);
-    const [botMessage, setMessage]=useState(`Hello, Vika! I'm a Bot!`)
+    const [botMessage, setMessage]=useState(`Hello, Vika! I'm a Bot!`);
+    const [chatList, setChatList]=useState([{id:1, name:"default"}, {id:2, name:"chat 2"}])
 
     const loadBotMessage = ()=>{
         setMessageList([...messageList, {text: botMessage, author: "Bot"}])
     };
+
 
     useEffect(() => {
         setTimeout(loadBotMessage,1500);
@@ -35,17 +45,31 @@ export default function MessageList(){
 
 
     return ( 
-        <div>
+    <div>
         <form onSubmit={handleSubmit}>
-            <input class="placeholder__text" placeholder="Your text" value={input} onChange={handleChange}/>
-            <input class="placeholder__text" placeholder="Author" value={name} onChange={handleAuthorChange}/>
-            <button class="submit__button" type="submit">Send message</button>
+            <div class="form_f"><TextField id="outlined-basic" autoFocus label="Your text" variant="outlined" value={input} onChange={handleChange}/></div>
+            <div class="form_f"><TextField id="outlined-basic"  label="Author" variant="outlined" value={name} onChange={handleAuthorChange}/></div>
+            <Button variant="contained" color="secondary" type="submit">Send message</Button>
         </form>
-        <ul>
-            {messageList.map((item,index)=>(
-                <li key={index}>{item.text}, author: {item.author}</li>
-            ))}
-        </ul>
+        <div class="form_ul">
+            <div class="form_ul_list">
+                <List >
+                    {chatList.map((item, index)=>(
+                                        <ListItem key={index}>
+                                        <ListItemText primary={item.name} secondary={item.id} />
+                                    </ListItem> 
+                    ))}          
+            
+                </List>
+            </div>
+            <div class="form_ul_list">
+                <ul>
+                    {messageList.map((item,index)=>(
+                        <li key={index}>{item.text}, author: {item.author}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     </div>
     )
 
